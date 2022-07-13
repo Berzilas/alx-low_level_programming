461c329d4f191f08cfdc0e926ae7307491868cde
Berzilas <ebersabeh@gmail.com>
2022-07-13 14:22:24 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /**
- * main - Entry point
- * Return: Always 0 (sucess)
- */
+* main - Entry point
+* Return: Always 0 (sucess)
+*/
 int main(void)
 { 
 char items[] = "\"Programming is like building a multilingual puzzle";
