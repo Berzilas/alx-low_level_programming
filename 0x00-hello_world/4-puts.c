7a2b30b835ca0c8030f81b31e3320c7432850970
Berzilas <ebersabeh@gmail.com>
2022-07-13 14:19:22 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#include <string.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (sucess)
+ */
+int main(void)
+{ 
+char items[] = "\"Programming is like building a multilingual puzzle";
+puts(items);
+return (0);
+}
